43892655f9dfcf84d2f18edb77bb26b55600e47a
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-18 14:25:42 -0600

    added POST request and validations

--- app.js
+++ app.js
@@ -11,19 +11,30 @@ app.listen(port, () => console.log(`Server online at port ${port}`));
 
 // Server Request Handling
 
+function validateGenre(genre) {
+    const schema = { genre: Joi.string().required() };
+    return Joi.validate(genre, schema);
+}
+
 // GET all genres
-app.get('/api/genres', (req, res) => { 
-    res.send(genres);
-});
+app.get('/api/genres', (req, res) => res.send(genres));
 
 // GET one genre
 app.get('/api/genres/:id', (req, res) => { 
     let genre_id = parseInt(req.params.id);
     let genre = genres.find(g => g.id === genre_id);
-    (genre) ? res.send(genre) : res.status(404).send(`Could not find ${genre_id}`);
+    (genre) ? res.send(genre) : res.status(404).send(`Could not find ${genre_id}`); 
+    // send genre if found, 404 and error message if not
 });
 
 // POST one genre
 app.post('/api/genres', (req, res) => {
-
+    const {error} = validateGenre(req.body); if (error) return res.status(400).send(error.details[0].message);
+    let genre = { id: genres.length, genre: req.body.genre };
+    genres.push(genre);
+    res.send(genre);
 });
+
+
+
+
