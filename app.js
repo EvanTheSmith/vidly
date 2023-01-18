a8edc7000da76151696119c18eadb90bf724d3ee
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-18 13:35:37 -0600

    setup port and server listening

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const genres = require('./genres.js');
 const Joi = require('joi'); // validations
 app.use(express.json()); // try commenting this out later to see what breaks
 
+// Server Setup
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server online at port ${port}`));
+
+// Server Request Handling
