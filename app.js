5969d12b04b006a0bf9269df51d23d54a44e282c
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-18 12:56:20 -0600

    setup required in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Required 
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+app.use(express.json()); // try commenting this out later to see what breaks
+
+
+
