afc5048867f13b34e817c092d2580053c3c93198
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-24 16:09:56 -0600

    began migration of routes to routes folder

--- app.js
+++ app.js
@@ -1,59 +1,13 @@
 // Required 
 const express = require('express');
 const app = express();
-const genres = require('./genres.js');
 const Joi = require('joi'); // validations
+const genre_router = require('./routes/genres_route.js');
+
 app.use(express.json()); // try commenting this out later to see what breaks
+app.use('api/genres', genre_router);
 
 // Server Setup
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server online at port ${port}`));
 
-// Server Request Handling
-
-function validateGenre(genre) {
-    const schema = { genre: Joi.string().required() };
-    return Joi.validate(genre, schema);
-}
-
-// GET all genres
-app.get('/api/genres', (req, res) => res.send(genres));
-
-// GET one genre
-app.get('/api/genres/:id', (req, res) => { 
-    let genre_id = parseInt(req.params.id);
-    let genre = genres.find(g => g.id === genre_id);
-    (genre) ? res.send(genre) : res.status(404).send(`Could not find ${genre_id}`); 
-    // send genre if found, 404 and error message if not
-});
-
-// POST one genre
-app.post('/api/genres', (req, res) => {
-    const {error} = validateGenre(req.body); if (error) return res.status(400).send(error.details[0].message);
-    let genre = { id: genres.length, genre: req.body.genre };
-    genres.push(genre);
-    res.send(genre);
-});
-
-// PUT (EDIT) one genre
-app.put('/api/genres/:id', (req, res) => {
-    let genre_id = parseInt(req.params.id);
-    let genre = genres.find(g => g.id === genre_id);
-    if (!genre) return res.status(404).send(`Could not find ${genre_id}`); 
-
-    const {error} = validateGenre(req.body); if (error) return res.status(400).send(error.details[0].message);
-
-    genre.genre = req.body.genre;
-    res.send(genre);
-});
-
-// DELETE one genre
-app.delete('/api/genres/:id', (req, res) => {
-    let genre_id = parseInt(req.params.id);
-    let genre = genres.find(g => g.id === genre_id);
-    if (!genre) return res.status(404).send(`Could not find ${genre_id}`); 
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-    res.send(genre);
-});
---
+++ routes/genres_route.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const genres = require('../genres.js');
+
+function validateGenre(genre) {
+    const schema = { genre: Joi.string().required() };
+    return Joi.validate(genre, schema);
+}
+
+// GET all genres
+router.get('/', (req, res) => res.send(genres));
+
+// GET one genre
+router.get('/:id', (req, res) => { 
+    let genre_id = parseInt(req.params.id);
+    let genre = genres.find(g => g.id === genre_id);
+    (genre) ? res.send(genre) : res.status(404).send(`Could not find ${genre_id}`); 
+    // send genre if found, 404 and error message if not
+});
+
+// POST one genre
+router.post('/', (req, res) => {
+    const {error} = validateGenre(req.body); if (error) return res.status(400).send(error.details[0].message);
+    let genre = { id: genres.length, genre: req.body.genre };
+    genres.push(genre);
+    res.send(genre);
+});
+
+// PUT (EDIT) one genre
+router.put('/:id', (req, res) => {
+    let genre_id = parseInt(req.params.id);
+    let genre = genres.find(g => g.id === genre_id);
+    if (!genre) return res.status(404).send(`Could not find ${genre_id}`); 
+
+    const {error} = validateGenre(req.body); if (error) return res.status(400).send(error.details[0].message);
+
+    genre.genre = req.body.genre;
+    res.send(genre);
+});
+
+// DELETE one genre
+router.delete('/:id', (req, res) => {
+    let genre_id = parseInt(req.params.id);
+    let genre = genres.find(g => g.id === genre_id);
+    if (!genre) return res.status(404).send(`Could not find ${genre_id}`); 
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+    res.send(genre);
+});
+
+module.exports = router;
