52fa2b88602eaa852c648932e846442f14f841ab
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-24 16:15:58 -0600

    completed migration of routes to routes folder

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const app = express();
 const Joi = require('joi'); // validations
-const genre_router = require('./routes/genres_route.js');
-
-app.use(express.json()); // try commenting this out later to see what breaks
-app.use('api/genres', genre_router);
+const genre_router = require('./routes/genre_router.js');
 
 // Server Setup
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server online at port ${port}`));
 
+// Middleware
+app.use(express.json()); // try commenting this out later to see what breaks
+app.use('/api/genres', genre_router);
