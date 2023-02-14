887aeb7ba3198e3cf9b4788585b10c3289d65184
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-14 00:02:01 -0600

    potentially built compelte genre_creator test function

--- genre_creator.js
+++ genre_creator.js
@@ -1,21 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useUnifiedTopology: true })
 
+const genreSchema = new mongoose.Schema(
+    { genre: { type: String, required: true } }
+);
 
-async function createCourse() {
-    const course = new Course({ // creates an individual course from the class
-        name: 'Angular Course',
-        category: 'one',
-        price: '15.5',
-        author: 'Evan',
-        tags: ['SQL', 'backend'], // this complex value is only possible in a document/noSQL database like MongoDB
-        isPublished: true
-    });
-    try {
-        const result = await course.save(); // save to database asynchronously; use await to wait for result
-        console.log(result);
-     }
-     catch(exception) { // block is executed in event of validation error or any failure in try block
-        for (error in exception.errors) {
-            console.log(exception.errors[error].message);
-        }
-     }
+async function createGenre() {
+    const genre = new Genre({ type: 'horror', });
+    try { const result = await genre.save(); console.log(result); }
+    catch(exception) { for (error in exception.errors) { console.log(exception.errors[error].message); } }
 }
