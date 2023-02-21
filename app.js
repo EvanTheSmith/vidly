8d19532dd609ca8ae610810e83ad646425e83e40
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-20 23:48:34 -0600

    added import for customer_router and app.use of it in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const genre_router = require('./routes/genre_router.js');
+const customer_router = require('./routes/customer_router.js');
 
 // Server Setup
 const port = process.env.PORT || 3000;
@@ -10,3 +11,4 @@ app.listen(port, () => console.log(`Server online at port ${port}`));
 // Middleware
 app.use(express.json()); // try commenting this out later to see what breaks
 app.use('/api/genres', genre_router);
+app.use('/api/customers', customer_router);
