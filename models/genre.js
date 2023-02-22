03859f898b5aff05d4eabe8b746f2594c8919234
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-21 23:00:08 -0600

    relocating Genre model into its own module under models/genre too

---
+++ models/genre.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({ genre: { type: String, required: true } }));
+
+function validateGenre(genre) {
+    const schema = { genre: Joi.string().required() };
+    return Joi.validate(genre, schema);
+}
+
+exports.Genre = Genre;
+exports.validate = validateGenre;
