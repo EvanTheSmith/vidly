5e3171f7da1a27d178190ee9d434f8504fa71936
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-02-27 23:54:39 -0600

    added comment to separate function from model creation in genre.js

--- models/genre.js
+++ models/genre.js
@@ -6,6 +6,8 @@ const Joi = require('joi');
 
 const Genre = mongoose.model('Genre', new mongoose.Schema({ genre: { type: String, required: true } }));
 
+// validate function : to be exported separately
+
 function validateGenre(genre) {
     const schema = { genre: Joi.string().required() };
     return Joi.validate(genre, schema);
